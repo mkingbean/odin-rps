001cd9702a04bc02a7ab1e4568ffe56a801b4425
matt <git@mking.io>
2025-10-25 11:53:22 +0100

    Write the logic to play 5 rounds and declare a winner

--- script.js
+++ script.js
@@ -22,40 +22,48 @@ function getHumanChoice() {
     }
 }
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-//playRound
-// Save the human choice to a variable
-// Save the computer choice to a variable
-// IF human choice = computer choice log "draw"
-// IF human wins THEN increment human score and log message
-// IF computer wins THEN increment computer score and log message
-
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-        window.console.log("It's a draw!");
-        return;
-    } else if (humanChoice === "rock" && computerChoice === "scissors"
-        || humanChoice === "paper" && computerChoice === "rock"
-        || humanChoice === "scissors" && computerChoice === "paper") {
-            window.console.log("You win!");
-            humanScore += 1;
+    function playRound() {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        if (humanChoice === computerChoice) {
+            window.console.log("It's a draw!");
             return;
-    } else if (humanChoice === "rock" && computerChoice === "paper"
-        || humanChoice === "paper" && computerChoice === "scissors"
-        || humanChoice === "scissors" && computerChoice === "rock") {
-            window.console.log("You lose!");
-            computerScore += 1;
+        } else if (humanChoice === "rock" && computerChoice === "scissors"
+            || humanChoice === "paper" && computerChoice === "rock"
+            || humanChoice === "scissors" && computerChoice === "paper") {
+                window.console.log("You win!");
+                humanScore += 1;
+                return;
+        } else if (humanChoice === "rock" && computerChoice === "paper"
+            || humanChoice === "paper" && computerChoice === "scissors"
+            || humanChoice === "scissors" && computerChoice === "rock") {
+                window.console.log("You lose!");
+                computerScore += 1;
+                return;
+        } else {
+            console.log("Invalid choice");
             return;
-    } else {
-        console.log("Invalid choice");
-        return;
+        }
     }
 
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+    playRound();
+
+    switch (true) {
+        case humanScore > computerScore:
+            console.log("You win the whole game!");
+            return;
+        case humanScore < computerScore:
+            console.log("You lose the whole game");
+            return;
+        case humanScore === computerScore:
+            console.log("The whole game is a draw!");
+    }
 }
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
