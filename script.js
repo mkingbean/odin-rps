4bd056e200c4e51afd58fc9f555ec2c7c9a200c5
matt <git@mking.io>
2025-10-25 11:39:03 +0100

    Write the logic to play a single round

--- script.js
+++ script.js
@@ -5,13 +5,10 @@ function getComputerChoice() {
     switch (randomInt) {
         case 0:
             return "rock";
-            break
         case 1:
             return "paper";
-            break
         case 2:
             return "scissors";
-            break
     }
 }
 
@@ -22,9 +19,43 @@ function getHumanChoice() {
         case "paper":
         case "scissors":
             return humanChoice;
-            break
     }
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
+//playRound
+// Save the human choice to a variable
+// Save the computer choice to a variable
+// IF human choice = computer choice log "draw"
+// IF human wins THEN increment human score and log message
+// IF computer wins THEN increment computer score and log message
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        window.console.log("It's a draw!");
+        return;
+    } else if (humanChoice === "rock" && computerChoice === "scissors"
+        || humanChoice === "paper" && computerChoice === "rock"
+        || humanChoice === "scissors" && computerChoice === "paper") {
+            window.console.log("You win!");
+            humanScore += 1;
+            return;
+    } else if (humanChoice === "rock" && computerChoice === "paper"
+        || humanChoice === "paper" && computerChoice === "scissors"
+        || humanChoice === "scissors" && computerChoice === "rock") {
+            window.console.log("You lose!");
+            computerScore += 1;
+            return;
+    } else {
+        console.log("Invalid choice");
+        return;
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
