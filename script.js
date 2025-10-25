6447928d1a46a632df01782e12d6f82133363970
matt <git@mking.io>
2025-10-25 10:17:29 +0100

    Create getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello, world.");
+
+function getComputerChoice() {
+    let randomInt = Math.floor(Math.random() * 3);
+    switch (randomInt) {
+        case 0:
+            return "Rock";
+        case 1:
+            return "Paper";
+        case 2:
+            return "Scissors";
+    }
+}
