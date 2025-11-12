3a42bede1886818a2d24337e933b640300fdc044
matt <git@mking.io>
2025-11-12 11:36:44 +0000

    Implement simple UI

--- script.js
+++ script.js
@@ -1,41 +1,9 @@
-function getHumanChoice() {
-    let humanChoice = window.prompt("Rock, Paper, or Scissors?").toLowerCase();
-    return humanChoice;
-}
-
-function getComputerChoice() {
-    let randomInt = Math.floor(Math.random() * 3);
-    switch (randomInt) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissors";
-    }
-}
-
-function calculateOverallWinner(humanScore, computerScore) {
-    switch (true) {
-        case humanScore > computerScore:
-            console.log("Congratulations, you've won the game!");
-            return;
-        case humanScore < computerScore:
-            console.log("Bad luck, you've lost the game!");
-            return;
-        case humanScore === computerScore:
-            console.log("It's a draw!");
-    }
-}
-
 function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
 
-    function playRound() {
+    function playRound(event) {
 
-        function choicesSummary() {
-            console.log(`You chose ${humanChoice} and the computer chose ${computerChoice}.`);
+        function displayChoices() {
+            roundChoices.innerText = `You chose ${humanChoice} and the computer chose ${computerChoice}.`;
         }
 
         function calculateRoundWinner() {
@@ -48,39 +16,88 @@ function playGame() {
                 || humanChoice === "scissors" && computerChoice === "rock";
 
             if (humanChoice === computerChoice) {
-            console.log("No-one wins any points!");
+            roundWinner.innerText = "No-one wins any points!";
             } else if (isHumanWin) {
-                console.log("You win one point!");
+                roundWinner.innerText = "You win one point!";
                 humanScore++;
             } else if (isComputerWin) {
-                console.log("The computer wins one point!");
+                roundWinner.innerText = "The computer wins one point!";
                 computerScore++;
             } else {
-                console.log(`"${humanChoice}" is not a valid choice.`);
+                roundWinner.innerText = `"${humanChoice}" is not a valid choice.`;
             }
         }
 
-        function pointsSummary() {
-            console.log(`You have ${humanScore} points and the computer has ${computerScore} points.`);
+        function updateScorecard() {
+            scorecard.innerText = `You have ${humanScore} points and the computer has ${computerScore} points.`;
+        }
+
+        function displayResult(message) {
+            // A delay of 50ms ensures that the page is repainted before the alert, which pauses the execution
+            setTimeout(() => {
+                window.alert(message);
+                humanScore = 0;
+                computerScore = 0;
+                roundChoices.innerText = "";
+                roundWinner.innerText = "";
+                scorecard.innerText = "";
+            }, 50);
+        }
+
+        function calculateOverallWinner() {
+            switch (true) {
+                case humanScore > computerScore:
+                    displayResult("Congratulations, you've won the game!");
+                    return;
+                case humanScore < computerScore:
+                    displayResult("Bad luck, you've lost the game!");
+                    return;
+                case humanScore === computerScore:
+                    displayResult("It's a draw!");
+            }
+        }
+
+        function checkForWinner() {
+            if (humanScore === 5 || computerScore === 5) calculateOverallWinner();
+        }
+
+        function getHumanChoice() {
+            return event.target.getAttribute("id");
+        }
+
+        function getComputerChoice() {
+            let randomInt = Math.floor(Math.random() * 3);
+            switch (randomInt) {
+                case 0:
+                    return "rock";
+                case 1:
+                    return "paper";
+                case 2:
+                    return "scissors";
+            }
         }
 
         const humanChoice = getHumanChoice();
         const computerChoice = getComputerChoice();
 
-        choicesSummary();
+        displayChoices();
         calculateRoundWinner();
-        pointsSummary();
+        updateScorecard();
+        checkForWinner();
 
     }
 
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
+    let humanScore = 0;
+    let computerScore = 0;
 
-    calculateOverallWinner(humanScore, computerScore);
+    const buttons = document.querySelectorAll("button");
+    const roundChoices = document.querySelector("#choices");
+    const roundWinner = document.querySelector("#round-winner")
+    const scorecard = document.querySelector("#scorecard");
 
+    for (const button of buttons) {
+        button.addEventListener("click", playRound);
+    }
 }
 
 playGame();
